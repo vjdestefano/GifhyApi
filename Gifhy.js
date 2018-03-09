d9b80c25ec5fd709455c935821237bd760b43d1f
Vinny DeStefano <vjdestefano500@gmail.com>
2018-03-09 11:15:49 -0500

    this is working

--- Gifhy.js
+++ Gifhy.js
@@ -1,19 +1,80 @@
 //Vincent DeStefano
 
+var searchOrRandom = true;
+var randomTerm = false;
+
+
+$(".trending").on("click",function(event){
+console.log(this);
+
+ var trendingSearchUrl = "https://api.giphy.com/v1/gifs/trending?api_key=af812ge2IP7qhl9AEIkTMPREE6ZFyTyq&limit=25&rating=PG-13"; 
+
+
+//make limit number a variable and increment that once the trending button is clicked returns the same trending gifs not new ones
+
+
+$.ajax({
+  url: trendingSearchUrl,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+
+  for(var i  = 0; i < 25; i++){
+        
+    var divTrending = $("<div>");
+    divTrending.addClass("col-3");
+
+    var imageStillTrending = response.data[i].images.fixed_height_still.url;
+    console.log(imageStillTrending);
+    var previewGifTrending = response.data[i].images.preview_gif.url;
+
+    
+
+  var imageDivTrending = $("<img>");
+  imageDivTrending.addClass("col-3 images");
+  imageDivTrending.attr("src", imageStillTrending);
+  
+  $("#test").append(imageDivTrending);
+
+  console.log(imageDivTrending);
+  }
+
+
+
+  });
+
+
+});
+
 $("#find-gif").on("click", function(event) {
   event.preventDefault();
 
   var search = $("#test-input").val();
+  var rating = $("#selected-Rating").val();
+  var searchingFor = '';
+
   console.log(search);
+  console.log(rating);
+  var count = $("#selected-count").val();
+  console.log(count);
 
-  var count = 5;
+if($("#find-gif").hasClass("random bg-warning")){
 
+  var querySearchURL = "https://api.giphy.com/v1/gifs/random?api_key=af812ge2IP7qhl9AEIkTMPREE6ZFyTyq&tag=&limit=25&offset=0rating=" + rating;
+
+    console.log(querySearchURL);
+}
+
+if($("#find-gif").hasClass("search")){
+
+  searchingFor = "search?"
   var querySearchURL =
-    "https://api.giphy.com/v1/gifs/search?api_key=af812ge2IP7qhl9AEIkTMPREE6ZFyTyq&q=" +
+    "https://api.giphy.com/v1/gifs/" + searchingFor + "api_key=af812ge2IP7qhl9AEIkTMPREE6ZFyTyq&q=" +
     search +
-    "&limit=25&offset=0&rating=PG&lang=en";
-
-  console.log(querySearchURL);
+    "&limit=" + count + "&offset=0&rating=" + rating + "&lang=en";
+    console.log(querySearchURL);
+}
+  
 
   $.ajax({
     url: querySearchURL,
@@ -21,30 +82,47 @@ $("#find-gif").on("click", function(event) {
   }).then(function(response) {
     console.log(response);
 
-    for(var i  = 0; i < count; i++){
+    if($("#find-gif").hasClass("random bg-warning")){
+      var testImage = response.data.images.fixed_height_still.url;
+        console.log(testImage);
+        $("#test").append("<img  class = 'images' src =" + testImage + ">");
+    }
+   
+    if($("#find-gif").hasClass("search")){
       
-      var divSection = $("<div>");
-      divSection.addClass("col-4");
-
-      var imageStill = response.data[i].images.fixed_height_still.url;
-
-      var previewGif = response.data[i].images.preview_gif.url;
-      console.log(imageStill);
-
-    var imageDiv = $("<img>");
-    imageDiv.addClass("col-4 images");
-    imageDiv.attr("src", imageStill);
-    imageDiv.data("imageNum", previewGif);
-    
-    $("#test").append(imageDiv);
-
-    console.log(imageDiv);
-    
-    
+      for(var i  = 0; i < count; i++){
+        
+        var divSection = $("<div>");
+        divSection.addClass("col-3");
+  
+        var imageStill = response.data[i].images.fixed_height_still.url;
+        console.log(imageStill);
+        var previewGif = response.data[i].images.preview_gif.url;
+  
+        var testRating = response.data[i].rating;
+        console.log(testRating);
+        
+  
+      var imageDiv = $("<img>");
+      imageDiv.addClass("col-3 images");
+      imageDiv.attr("src", imageStill);
+      
+      $("#test").append(imageDiv);
+  
+      console.log(imageDiv);
+      }
+     
     }
     
   });
 });
+
+$("#randomTest").on("click", function(event) {
+  $("#find-gif").toggleClass("search");
+  $("#find-gif").toggleClass("random bg-warning border-danger ");
+
+});
+
 
 $(document).on("click", ".images", function () {
 
@@ -58,7 +136,6 @@ $(document).on("click", ".images", function () {
     $(this).addClass('playing');
     $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
   }
-
 
 });
 
